84b8f7487bbe0db5f7e79fec840ae2a3933e0e3a
BIn <BIn@DESKTOP-44OGHB2>
2023-09-22 10:12:54 +0900

    4번완료

---
+++ quiz1_bin/src/main/java/edu/mit/controller/QuizController.java
@@ -0,0 +1,15 @@
+package edu.mit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class QuizController {
+	
+	@GetMapping("/quiz/quiz1")
+	void quizjsp() {
+		
+		
+	}
+
+}
